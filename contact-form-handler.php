90b9e49a7c9be672456da220c5868e519a06987a
gkazragis <36165559+gkazragis@users.noreply.github.com>
2018-05-25 12:42:33 +0300

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -11,8 +11,8 @@ if(empty($_POST['name'])  ||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$menu_list = $_POST['meniu'];
-$message = $_POST['message'];
+// $menu_list = $_POST['meniu'];
+// $message = $_POST['message'];
 // also add dates !!!
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
@@ -25,9 +25,10 @@ if( empty($errors))
 {
 	$to = $myemail;
 	$email_subject = "Contact form submission: $name";
-  
+
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message \n Meniu: $menu_list";
+	// " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message \n Meniu: $menu_list";
+  " Here are the details:\n Name: $name \n Email: $email_address \n ";
 
 	$headers = "From: $myemail\n";
 	$headers .= "Reply-To: $email_address";
